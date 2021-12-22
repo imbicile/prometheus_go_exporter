f87487a710cef2d7fcf5147197d2e66acec912c3
Stanislav Kononov <35522330+imbicile@users.noreply.github.com>
2021-12-22 15:06:46 +0300

    Update go_exporter.go

--- go_exporter.go
+++ go_exporter.go
@@ -2,38 +2,40 @@ package main
 
 import (
 	"fmt"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 	"net/http"
 	"os"
 )
 
-var (
-	// exporter conf
-	addr = "127.0.0.1"
-	port = "8080"
-)
-
 func main() {
-	fmt.Println("Server run at " + addr + ":" + port)
+	var (
+		// exporter address and port
+		addr = kingpin.Flag("listen-address", "Exporter address.").Default("0.0.0.0").String()
+		port = kingpin.Flag("listen-port", "Exporter port.").Default("8080").String()
+	)
+
+	HelloServer := func(w http.ResponseWriter, r *http.Request) {
+
+		hostname, err := os.Hostname()
+		if err != nil {
+			panic(err)
+		}
+
+		sys_info := func(exp_name, exp_val string, exp_descr string) {
+			fmt.Fprintln(w, "# HELP Debug "+exp_name+exp_descr)
+			fmt.Fprintln(w, exp_name+"{nodename=\""+hostname+"\"} "+exp_val)
+		}
+		// print to web sysinfo
+		sys_info("custom_exporter_addr", *addr, " Custom exporter running address.")
+		sys_info("custom_exporter_port", *port, " Custom exporter running port.")
+	}
+
+	kingpin.CommandLine.UsageWriter(os.Stdout)
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	fmt.Println("Server run at " + *addr + ":" + *port)
 	http.HandleFunc("/metrics", HelloServer)
-	http.ListenAndServe(addr+":"+port, nil)
-}
+	http.ListenAndServe(*addr+":"+*port, nil)
 
-func Out(export string) {
-
-}
-
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-
-	hostname, err := os.Hostname()
-	if err != nil {
-		panic(err)
-	}
-
-	sys_info := func(exp_name, exp_val string, exp_descr string) {
-		fmt.Fprintln(w, "# HELP Debug "+exp_name+exp_descr)
-		fmt.Fprintln(w, exp_name+"{nodename=\""+hostname+"\"} "+exp_val)
-	}
-	// print to web
-	sys_info("custom_exporter_addr", addr, " Custom exporter running address.")
-	sys_info("custom_exporter_port", port, " Custom exporter running port.")
 }
